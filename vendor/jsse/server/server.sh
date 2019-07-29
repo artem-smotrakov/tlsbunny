92a49018544a27f7bf21e6c6fe5622b80a11bff3
Artem Smotrakov <artem.smotrakov@gmail.com>
2019-07-29 22:28:50 +0200

    Added scripts to run fuzzing agains JSSE

---
+++ vendor/jsse/server/server.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm -rf fuzzer.log
+
+java -cp ../../../target/tlsbunny-1.0-SNAPSHOT-all.jar \
+    -Djavax.net.ssl.keyStore=../../../certs/keystore \
+    -Djavax.net.ssl.keyStorePassword=passphrase \
+        com.gypsyengineer.tlsbunny.poc.jsse.JavaTls13Server 2>&1 \
+            | tee server.log
