d411e9283a6e1db6ce5c21368615cab928f1c955
Artem Smotrakov <artem.smotrakov@gmail.com>
2019-10-05 22:18:12 +0200

    Use JAVA_HOME to start JSSE fuzzing

--- vendor/jsse/server/fuzzer.sh
+++ vendor/jsse/server/fuzzer.sh
@@ -2,10 +2,6 @@
 
 rm -rf fuzzer.log
 
-java -cp ../../../target/tlsbunny-1.0-SNAPSHOT-all.jar \
+${JAVA_HOME}/bin/java -cp ../../../target/tlsbunny-1.0-SNAPSHOT-all.jar \
     com.gypsyengineer.tlsbunny.tls13.client.fuzzer.DeepHandshakeFuzzyClient 2>&1 \
         | tee fuzzer.log
-
-if grep AddressSanitizer server.log ; then
-    echo "Achtung! AddressSanitizer found something!"
-fi
--- vendor/jsse/server/server.sh
+++ vendor/jsse/server/server.sh
@@ -2,7 +2,7 @@
 
 rm -rf fuzzer.log
 
-java -cp ../../../target/tlsbunny-1.0-SNAPSHOT-all.jar \
+${JAVA_HOME}/bin/java -cp ../../../target/tlsbunny-1.0-SNAPSHOT-all.jar \
     -Djavax.net.ssl.keyStore=../../../certs/keystore \
     -Djavax.net.ssl.keyStorePassword=passphrase \
         com.gypsyengineer.tlsbunny.poc.jsse.JavaTls13Server 2>&1 \
